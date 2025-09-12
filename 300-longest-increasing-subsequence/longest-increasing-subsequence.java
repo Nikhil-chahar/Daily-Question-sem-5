196f8f1ba7a61a918d28ce38dfe12ed76fbcc35a
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-09-13 02:37:52 +0530

    Time: 89 ms (23.47%) | Memory: 44.7 MB (33.42%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,15 +1,75 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
-        int dp[] =  new int[arr.length];
-        Arrays.fill(dp,1);
-        for(int i=1;i<dp.length;i++){
-            for(int j=i-1;j>=0;j--){
+
+        int sol[] = new int[arr.length];
+        Arrays.fill(sol,1);
+
+        for(int i=0;i<arr.length;i++){
+            // int val = arr[i];
+            int ma=0;
+            for(int j = i-1;j>=0;j--){
+
                 if(arr[i] > arr[j]){
-                    int x = dp[j];
-                    dp[i] = Math.max(dp[i],x+1);
+                    ma = Math.max(ma,sol[j]);
                 }
             }
+            sol[i]+=ma;
+        } 
+        int max=1;
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(sol[i]+" ");
+            if(sol[i] > max){
+                max = sol[i];
+            }
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return max;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int dp[] =  new int[arr.length];
+        // Arrays.fill(dp,1);
+        // for(int i=1;i<dp.length;i++){
+        //     for(int j=i-1;j>=0;j--){
+        //         if(arr[i] > arr[j]){
+        //             int x = dp[j];
+        //             dp[i] = Math.max(dp[i],x+1);
+        //         }
+        //     }
+        // }
+        // return Arrays.stream(dp).max().getAsInt();
     }
 }
