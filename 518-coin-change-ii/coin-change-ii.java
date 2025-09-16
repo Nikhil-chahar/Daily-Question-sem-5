7bd7febe17ebb9675c1c2bbb729b93368de7ddd0
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-09-16 09:04:43 +0530

    Time: 18 ms (19.29%) | Memory: 51.3 MB (9.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,27 +1,29 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int dp[][] = new int[amount+1][coins.length];
-        for(int a[] : dp){
+        for(int a[]: dp){
             Arrays.fill(a,-1);
         }
-        return coin_Change(coins,amount,0,dp);
+
+        return calc(amount,coins,0,dp);
     }
-    public int coin_Change(int coin[], int amount,int i,int dp[][]){
+    public int calc(int amount,int[] coins,int i,int dp[][]){
         if(amount == 0){
             return 1;
         }
-        if(i == coin.length){
+        if(i== coins.length){
             return 0;
         }
         if(dp[amount][i] != -1){
             return dp[amount][i];
         }
-        int inc = 0,exc =0;
-        if(amount >= coin[i]){
-            inc = coin_Change(coin, amount-coin[i], i,dp);
-        }
-        exc = coin_Change(coin, amount, i+1,dp);
-        return dp[amount][i] = inc+exc;
+        int inc =0, exc =0;
 
+        if(amount >= coins[i]){
+            inc = calc(amount-coins[i],coins,i,dp);
+        }
+        exc = calc(amount,coins,i+1,dp);
+
+        return dp[amount][i] = inc + exc;
     }
 }
