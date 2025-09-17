6aef2bd0fee3c379c070b53aca74a4b38261c205
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-09-17 12:08:31 +0530

    Time: 2 ms (99.54%) | Memory: 44.8 MB (33.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,74 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
-
-        int sol[] = new int[arr.length];
-        Arrays.fill(sol,1);
-
-        for(int i=0;i<arr.length;i++){
-            int ma=0;
-            for(int j = i-1;j>=0;j--){
-                if(arr[i] > arr[j]){
-                    ma = Math.max(ma,sol[j]);
-                }
+        return LIS(arr);
+    } 
+    public static int LIS(int arr[]){
+        int dp[] = new int[arr.length];
+        int len =1;
+        dp[0]= arr[0];
+        for(int i=1;i<dp.length;i++){
+            if(arr[i] > dp[len-1]){
+                dp[len] = arr[i];
+                len++;
+            }else{
+                int idx = BinarySearch(dp,0,len-1,arr[i]);
+                dp[idx] = arr[i];
             }
-            sol[i]+=ma;
-        } 
+        }
+        return len;
 
-        // int max=1;
-        // for(int i=0;i<arr.length;i++){
-        //     System.out.print(sol[i]+" ");
-        //     if(sol[i] > max){
-        //         max = sol[i];
-        //     }
-        // }
-        // return max;
-        return Arrays.stream(sol).max().getAsInt();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int dp[] =  new int[arr.length];
-        // Arrays.fill(dp,1);
-        // for(int i=1;i<dp.length;i++){
-        //     for(int j=i-1;j>=0;j--){
-        //         if(arr[i] > arr[j]){
-        //             int x = dp[j];
-        //             dp[i] = Math.max(dp[i],x+1);
-        //         }
-        //     }
-        // }
-        // return Arrays.stream(dp).max().getAsInt();
+    }
+    public static int BinarySearch(int dp[] ,int si,int ei,int item) {
+        int idx = 0;
+        while(si <= ei){
+            int mid = (si+ei)/2;
+            if(dp[mid]>= item){
+                idx = mid;
+                ei = mid-1;
+            }else{
+                si = mid +1;
+            }
+        }
+        return idx;
     }
 }
