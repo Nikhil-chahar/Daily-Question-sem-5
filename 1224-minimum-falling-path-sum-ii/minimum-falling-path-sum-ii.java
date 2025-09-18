1bcf954b4a0756da95f4bec010a643db8bbbe079
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-09-19 01:39:49 +0530

    Time: 201 ms (21.76%) | Memory: 51.6 MB (58.26%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int dp[][] =new int[matrix.length][matrix[0].length];
         for(int a[] : dp){
-            Arrays.fill(a,-66666);
+            Arrays.fill(a,-100);
         }
         int ans = Integer.MAX_VALUE;
         for(int col=0;col<matrix[0].length;col++){
@@ -10,23 +10,20 @@ class Solution {
         }
         return ans;
     }
-    public static int Minimum_falling(int[][] matrix, int cr,int cc,int dp[][]){
-        if(cr == matrix.length-1){
-            return matrix[cr][cc];
+    public static int Minimum_falling(int[][] grid, int cr,int cc,int dp[][]){
+       if(cr == grid.length-1){
+        return grid[cr][cc];
+       }
+       if(dp[cr][cc] != -100){
+        return dp[cr][cc];
+       }
+       int ans = Integer.MAX_VALUE;
+       for(int i=0;i<grid[0].length;i++){
+        if(cc != i){
+            ans = Math.min(ans,Minimum_falling(grid,cr+1,i,dp));
         }
-        if(dp[cr][cc] != -66666){
-            return dp[cr][cc];
-        }
-
-        int ans = Integer.MAX_VALUE;
-        for(int i=0;i<matrix[0].length;i++){
-            if(i == cc){
-                continue;
-            }
-            ans = Math.min(ans,Minimum_falling(matrix, cr+1, i,dp));
-        }
-
-        return dp[cr][cc] = ans + matrix[cr][cc];
+       }
+       return dp[cr][cc] = ans+grid[cr][cc];
 
     }
 }
