e2edf29e1f1963861352a937cc5896ab5133950e
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-10-07 23:32:14 +0530

    Time: 15 ms (37.43%) | Memory: 49.1 MB (18.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,28 +5,28 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        List<Integer> ll = new ArrayList<>();
-        LinkedHashMap<Integer, Integer> sortedMap = map.entrySet()
-            .stream()
-            .sorted((a, b) -> b.getValue().compareTo(a.getValue())) // compare values
-            .collect(Collectors.toMap(
-                Map.Entry::getKey,
-                Map.Entry::getValue,
-                (e1, e2) -> e1,
-                LinkedHashMap::new
-            )
-        );
+        int arr[][] = new int[map.size()][2];
+        int ind=0;
+
+        for(int key : map.keySet()){
+            arr[ind][0] = key;
+            arr[ind][1] = map.get(key);
+            ind++;
+        }
+        Arrays.sort(arr, (a,b) ->b[1]-a[1]);
+
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i][0] +" " +arr[i][1]);
+        // }
         
         int ans[] = new int[k];
-        for(int key : sortedMap.keySet()){
-            if(k-- > 0){
-                ll.add(key);
-            }
+        for(int i=0;i<k;i++){
+            ans[i] = arr[i][0];
         }
-        System.out.print(ll);
-        for(int i=0;i<ans.length;i++){
-            ans[i] = ll.get(i);
-        }
+        // System.out.print(ll);
+        // for(int i=0;i<ans.length;i++){
+        //     ans[i] = ll.get(i);
+        // }
         
         return ans;
     }
