7fb9f238901575e342503178ae7e41d1762cdd9b
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-10-09 00:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.98%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,34 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        for(int i =2;i<n-1;i++){
-            String s = Bin(n,i);
-            if(!ispalin(s)){
-                return false;
-            }
-        }
-        // System.out.print(Bin(10,2));
-        return true;
-
-    }
-    public String Bin(int val,int base){
-        StringBuilder sb = new StringBuilder();
-
-        while(val >0){
-            sb.append(Integer.toString(val%base));
-            val /= base;
-        }
-        return sb.reverse().toString();
-    }
-    public boolean ispalin(String s){
-        int si=0,ei=s.length()-1;
-
-        while(si < ei){
-            if(s.charAt(si) != s.charAt(ei)){
-                return false;
-            }
-            si++;
-            ei--;
-        }
-        return true;
+        return false;
     }
 }
