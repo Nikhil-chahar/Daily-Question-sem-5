2040900635654591791e4d29789a4dfe9c549972
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-10-09 18:04:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (23.43%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int integerReplacement(int n) {
+        return helper((long)n);
+    }
+    public int helper(long n){
+        if(n==1) return 0;
+
+        if(n%2 == 0) return 1+helper(n/2);
+        else if(n == 3 || n%4 == 1) return 1+ helper(n-1);
+        else return 1+helper(n+1);
+    }
+}
