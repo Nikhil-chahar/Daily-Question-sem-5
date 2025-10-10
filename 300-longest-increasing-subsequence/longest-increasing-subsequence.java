9cf3b664dd75c7833cc0f070ea6a03ae05b13ac9
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-10-10 15:01:48 +0530

    Time: 38 ms (70.44%) | Memory: 44.6 MB (42.74%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,34 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return LIS(nums);
-    }
-    public int LIS(int arr[]){
-        int dp[] = new int[arr.length];
-        int len=1;
-        dp[0] = arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(dp[len-1] < arr[i]){
-                dp[len] = arr[i];
-                len++;
-            }else{
-                int idx = BinarySearch(dp,0,len-1,arr[i]);
-                dp[idx] = arr[i];
+        int n=nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,1);
+
+        for(int i=1;i<n;i++){
+            int max = 0;
+            for(int j=i-1;j>=0;j--){
+                if(nums[j] < nums[i]){
+                    max = Math.max(dp[j],max);
+                }
+            }
+            dp[i] += max;
+        }
+        int max =0;
+        for(int i=0;i<n;i++){
+            if(max < dp[i]){
+                max = dp[i];
             }
         }
-        return len;
-    }
-    public int BinarySearch(int dp[],int si,int ei,int item){
-        int idx=0;
-        while(si <=ei){
-            int mid = (si+ei)/2;
-            if(dp[mid] >= item){
-                idx = mid;
-                ei = mid-1;
-            }
-            else{
-                si = mid+1;
-            }
-        }
-        return idx;
+        return max;
     }
 }
