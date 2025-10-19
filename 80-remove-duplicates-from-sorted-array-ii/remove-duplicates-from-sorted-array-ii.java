be876cf6f5a6433ef6ec89b68a007adec211c271
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-10-19 23:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (31.43%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,29 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Map<Integer,Integer> map = new LinkedHashMap<>();
+        int k=2;
 
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        int sum=0;
-        for(int key : map.keySet()){
-            if(map.get(key) >2){
-                sum +=2;
-            }else{
-                sum += map.get(key);
+        for(int i=2;i<nums.length;i++){
+            if(nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        int ind=0;
-        int i=0;
-        for(int key : map.keySet()){
-            if(map.get(key) >1){
-                nums[ind++] = key;
-                nums[ind++] = key;
-            }else{
-                nums[ind++] = key;
-            }
-        }
-        System.out.print(map);
-        return sum;
+        return k;
     }
 }
