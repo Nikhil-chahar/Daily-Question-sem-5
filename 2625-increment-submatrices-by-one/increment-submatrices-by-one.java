e26dfc592926159aeed974d83f337e2ce8caee21
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-11-14 21:39:45 +0530

    Time: 459 ms (19.74%) | Memory: 76.9 MB (23.25%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int mat[][] = new int[n][n];
+        
+        int grid[][] = new int[n][n];
 
-        for(int i[] : mat){
-            Arrays.fill(i,0);
-        }
-
-        for(int que[] : queries){
-            int rmin=que[0];
-            int cmin=que[1];
-            int rmax=que[2];
-            int cmax=que[3];
+        for(int q[] : queries){
+            int rmin = q[0];
+            int cmin = q[1];
+            int rmax = q[2];
+            int cmax = q[3];
 
             for(int i=rmin;i<=rmax;i++){
                 for(int j=cmin;j<=cmax;j++){
-                    mat[i][j]++;
+                    grid[i][j]++;
                 }
             }
 
         }
-        return mat;
+        return grid;
     }
 }
