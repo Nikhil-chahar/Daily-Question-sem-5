ff67dbdebaee927212d1891332f98ed03fa73caf
Nikhil chahar <nikhilchahar7302@gmail.com>
2025-12-26 21:14:20 +0530

    Time: 2 ms (91.13%) | Memory: 80.6 MB (28.63%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,17 +1,10 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        TreeSet<Integer> set1 = new TreeSet<>();
-        TreeSet<Integer> set2 = new TreeSet<>();
-        for(int i=0;i<nums1.length;i++){
-            set1.add(nums1[i]);
-        }
-        for(int i=0;i<nums2.length;i++){
-            set2.add(nums2[i]);
-        }
-        for(int key : set1){
-            if(set2.contains(key)){
-                return key;
-            }
+        int i=0,j=0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]) return nums1[i];
+            else if(nums1[i] > nums2[j]) j++;
+            else i++;
         }
         return -1;
     }
