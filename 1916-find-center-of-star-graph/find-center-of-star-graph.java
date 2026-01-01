e7b8943f373fe3456bbd4f4ef1c399357158f260
Nikhil chahar <nikhilchahar7302@gmail.com>
2026-01-01 12:15:26 +0530

    Time: 15 ms (19.75%) | Memory: 138.7 MB (5.26%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,31 +1,27 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
-            return edges[0][0];
+
+        int ed = edges.length + 1;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0; i <= ed; i++){
+            graph.add(new ArrayList<>());
         }
-        return edges[0][1];
 
-        // ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        // int ed = edges.length+1;
-        // for(int i=1;i<=ed;i++){
-        //     graph.add(i,new ArrayList<>()); 
-        // }
+        for(int[] val : edges){
+            int u = val[0];
+            int v = val[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
-        // for(int val[] : edges){
-        //     int u = val[0];
-        //     int v = val[1];
-        //     graph.get(u).add(v);
-        //     graph.get(v).add(u);
-        // }
-        // int max=0;
-        // int ans=0;
-        // for(int i=1;i<ed;i++){
-        //     int cnt =0;
-        //     for(int val : graph.get(i)){
-        //         cnt++;
-
-        //     }
-
-        // }
+        int max = 0, ans = 0;
+        for(int i = 1; i <= ed; i++){
+            if(graph.get(i).size() > max){
+                max = graph.get(i).size();
+                ans = i;
+            }
+        }
+        return ans;
     }
 }
