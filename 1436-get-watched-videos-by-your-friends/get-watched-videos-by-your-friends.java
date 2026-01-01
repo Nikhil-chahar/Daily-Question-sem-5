f7e5491421f3f6b2e7e3dbf2395652329be30c66
Nikhil chahar <nikhilchahar7302@gmail.com>
2026-01-01 23:18:06 +0530

    Time: 40 ms (14.96%) | Memory: 48.4 MB (11.40%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
+
+        PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)->{
+            return a.cost-b.cost;
+        });
+        pq.add(new Pair(id,0));
+
+        List<Integer>list = new ArrayList<>();
+        HashSet<Integer>visited = new HashSet<>();
+
+        while(!pq.isEmpty()){
+            Pair rm = pq.poll();
+
+            if(visited.contains(rm.vtx)){
+                continue;
+            }
+
+            visited.add(rm.vtx);
+
+            if(rm.cost==level){
+                list.add(rm.vtx);
+            }
+
+            for(int ngbr : friends[rm.vtx]){
+                if(!visited.contains(ngbr)){
+                    pq.add(new Pair(ngbr, rm.cost+1));
+                }
+            }
+        }
+
+        HashMap<String,Integer>freq = new HashMap<>();
+        for(int vtx : list){
+            for(String ch : watchedVideos.get(vtx)){
+                freq.put(ch , freq.getOrDefault(ch,0)+1);
+            }
+        }
+
+        List<String> ans = new ArrayList<>(freq.keySet());
+        Collections.sort(ans,(a,b)->{
+            if(freq.get(a)==freq.get(b)){
+                return a.compareTo(b);
+            }
+            return freq.get(a)-freq.get(b);
+        });
+
+        return ans;
+    }
+
+    class Pair{
+        int vtx;
+        int cost;
+        public Pair(int vtx, int cost){
+            this.vtx = vtx;
+            this.cost = cost;
+        }
+    }
+}
